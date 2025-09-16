7d18cbea015a69f9d5ecb7708e05cf272dc6e581
Priyanka Rode <122172168+Priyanka-Rode@users.noreply.github.com>
2025-09-16 13:30:02 +0530

    Add files via upload

---
+++ User_Management_System/src/main/java/com/example/User_Management_System/Exception/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package com.example.User_Management_System.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+public static final long serialVersionUID =1L;
+ public ResourceNotFoundException(String message)
+ {
+     super(message);
+ }
+
+}
